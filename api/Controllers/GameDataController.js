813dcf105a8491225b8e845587088c88ae56b311
phuong7421 <44965922+phuong7421@users.noreply.github.com>
2018-12-04 14:57:05 +0700

    Update GameDataController.js

--- api/Controllers/GameDataController.js
+++ api/Controllers/GameDataController.js
@@ -53,11 +53,48 @@ let parameters = req.body.result.parameters;
       let team = parameters.team;
       if (game_occurence == "previous")
       {
-        return res.json({
-                        speech: 'Cant find any  ',
-                        displayText: 'Cant find any  ',
+        //previous game
+        GameSchedule.find({opponent:team},function(err,games)
+        {
+          if (games)
+          {
+            var requiredGame;
+            for (var i=0; i < games.length; i++)
+            {
+                var game = games[i];
+var convertedCurrentDate = new Date();
+                var convertedGameDate = new Date(game.date);
+if (convertedGameDate > convertedCurrentDate)
+                {
+                  if(games.length > 1)
+                  {
+                    requiredGame = games[i-1];
+var winningStatement = "";
+                    if (requiredGame.isWinner)
+                    {
+                        winningStatement = "Kings won this match by "+requiredGame.score;
+                    }
+                    else {
+                      winningStatement = "Kings lost this match by "+requiredGame.score;
+                    }
+                    return res.json({
+                        speech: 'Last game between Kings and '+parameters.team+' was played on '+requiredGame.date+' .'+winningStatement,
+                        displayText: 'Last game between Kings and '+parameters.team+' was played on '+requiredGame.date+' .'+winningStatement,
                         source: 'game schedule'
                     });
+                    break;
+                  }
+                  else {
+                    return res.json({
+                        speech: 'Cant find any previous game played between Kings and '+parameters.team,
+                        displayText: 'Cant find any previous game played between Kings and '+parameters.team,
+                        source: 'game schedule'
+                    });
+                  }
+                }
+            }
+}
+});
       }
       else {
         return res.json({
